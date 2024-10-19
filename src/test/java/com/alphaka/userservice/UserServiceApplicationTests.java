0ad96fb4fc6f98387ac1c5fc21f645cd367306d0
chanhoim <cksgh201@naver.com>
2024-10-19 18:43:30 +0900

    AK-176 [Chore] 젠킨스파일, 도커파일 작성

--- src/test/java/com/alphaka/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/alphaka/userservice/UserServiceApplicationTests.java
@@ -2,7 +2,10 @@ package com.alphaka.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class UserServiceApplicationTests {
 
