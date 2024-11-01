34184529dbb0d259800337cab194de2a6402f9a6
chanhoim <cksgh201@naver.com>
2024-11-01 11:44:37 +0900

    AK-234 [Chore] 스프링 클라우드 쿠버네티스 적용 & 프로필 분리

--- src/test/java/com/alphaka/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/alphaka/userservice/UserServiceApplicationTests.java
@@ -2,10 +2,9 @@ package com.alphaka.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("develop")
+@ActiveProfiles("local")
 @SpringBootTest
 class UserServiceApplicationTests {
 
